13a520862ae4e3507176e8d906dd13b68445bd1f
JB <jb.roux@ymail.com>
2020-02-28 17:54:03 +0100

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# How to use
+
+## 1. Generate DTO and client files
+``mvn poc.graphql:generate-graphql-client-maven-plugin:generate``
+
+## 2. Execute the Main
+``clean compile exec:java``
---
+++ src/main/java/Main.java
@@ -0,0 +1,36 @@
+import superhero.GQLClient;
+import superhero.Operations;
+import superhero.QueryQuery;
+import superhero.QueryResponse;
+import superhero.SuperHeroQueryDefinition;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello");
+
+        testGraphQL();
+    }
+
+    private static void testGraphQL() throws Exception {
+        QueryQuery.AllHeroesInArgumentsDefinition searchArgs = s -> s.city("Los Angeles, CA");
+
+        SuperHeroQueryDefinition queryArgs = q -> q
+                .currentLocation()
+                .name()
+                .superPowers()
+                .teamAffiliations(tA -> tA
+                        .name()
+                        .members(m -> m.name())
+                );
+
+        QueryQuery query = Operations.query(q -> q
+                .allHeroesIn(searchArgs, queryArgs)
+        );
+
+        GQLClient client = new GQLClient(() -> "http://localhost:8080/graphql");
+        QueryResponse response = client.execute(query);
+
+        System.out.println(response.prettyPrintJson());
+    }
+}
